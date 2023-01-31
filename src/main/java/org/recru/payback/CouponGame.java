814ef5defa31374f78ff785fe858122d8ef39ddb
Franek Antoniak <franekant123@gmail.com>
2023-01-31 21:54:59 +0100

    stream solution

--- src/main/java/org/recru/payback/CouponGame.java
+++ src/main/java/org/recru/payback/CouponGame.java
@@ -1,59 +1,103 @@
 package org.recru.payback;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import java.util.function.Function;
 import java.util.stream.IntStream;
 
 public class CouponGame {
-	private static final int ROWS = 15;
-	private static final int COLS = 15;
-	private static final int[] ROUNDS = {25, 50, 100};
-	private int[][] checkerboard = new int[ROWS][COLS];
-	private Random random = new Random();
-
-	public void play() {
-		// Initialize checkerboard with 1 Pointee per square
-		IntStream.range(0, ROWS).forEach(i -> IntStream.range(0, COLS).forEach(j -> checkerboard[i][j] = 1));
-
-		// Play game rounds
-		for (int round = 1; round <= 100; round++) {
-			// Make Pointees jump to random adjacent square
-			for (int i = 0; i < ROWS; i++) {
-				for (int j = 0; j < COLS; j++) {
-					int newI = i + random.nextInt(3) - 1;
-					int newJ = j + random.nextInt(3) - 1;
-					if (newI >= 0 && newI < ROWS && newJ >= 0 && newJ < COLS) {
-						checkerboard[newI][newJ]++;
-						checkerboard[i][j]--;
-					}
-				}
-			}
-
-			// Check if current round is a redemption round
-			for (int redemptionRound : ROUNDS) {
-				if (round == redemptionRound) {
-					System.out.println("Round " + round + " - results:");
-					// Calculate number of points per coupon
-					int[] points = new int[ROWS * COLS];
-					int index = 0;
-					for (int i = 0; i < ROWS; i++) {
-						for (int j = 0; j < COLS; j++) {
-							points[index++] = checkerboard[i][j];
-						}
-					}
-					// Find coupon(s) with highest number of points
-					int maxPoints = 0;
-					for (int point : points) {
-						if (point > maxPoints) {
-							maxPoints = point;
-						}
-					}
-					System.out.println("Coupon(s) with highest points: " + maxPoints);
-				}
-			}
-		}
-	}
+	private final Board board = new Board();
+	private int roundCounter = 0;
 
 	public static void main(String[] args) {
 		new CouponGame().play();
+	}
+
+	public void play() {
+		simulate(25);
+		simulate(50);
+		simulate(100);
+	}
+
+	private void simulate(int finalSimulationRound) {
+		assert finalSimulationRound > roundCounter;
+		int rounds = finalSimulationRound - roundCounter;
+		System.out.println("Simulating game to round " + finalSimulationRound);
+		IntStream.range(0, rounds)
+				.forEach(i -> simulateRound());
+		roundCounter = finalSimulationRound;
+		System.out.println("* Cupon(s) with most pointees: " + board.getMaxPointees());
+	}
+
+	private void simulateRound() {
+		board.map.entrySet()
+				.stream()
+				.filter(entry -> entry.getValue().pointees > 0)
+				.forEach(entry -> entry.getValue()
+						.transferPointee(board.map.get(board.getRandomNeighbour(entry.getKey()))));
+	}
+
+	private static class Board {
+		private static final int ROWS = 15;
+		private static final int COLS = 15;
+		private final Random random = new Random();
+		private final Map<Coordinate, Coupon> map = new HashMap<>();
+
+		public Board() {
+			IntStream.range(0, ROWS)
+					.forEach(i -> IntStream.range(0, COLS)
+							.forEach(j -> map.put(new Coordinate(i, j), new Coupon())));
+		}
+
+		public int getMaxPointees() {
+			return map.values()
+					.stream()
+					.max(Coupon::compareTo)
+					.orElse(new Coupon()).pointees;
+		}
+
+		public Coordinate getRandomNeighbour(Coordinate coord) {
+			int neighbors = getNumberOfNeighbours(coord);
+			return IntStream.rangeClosed(-1, 1)
+					.mapToObj(i -> IntStream.rangeClosed(-1, 1)
+							.mapToObj(j -> new Coordinate(coord.row + i, coord.col + j)))
+					.flatMap(Function.identity())
+					.filter(map::containsKey)
+					.filter(coordinate -> !coordinate.equals(coord))
+					.skip(random.nextInt(neighbors))
+					.findFirst()
+					.orElseThrow();
+		}
+
+		private int getNumberOfNeighbours(Coordinate coord) {
+			// on corners
+			if ((coord.row == 0 || coord.row == 14) && (coord.col == 0 || coord.col == 14)) {
+				return 3;
+			}
+			// on edges
+			if (coord.row == 0 || coord.row == 14 || coord.col == 0 || coord.col == 14) {
+				return 5;
+			}
+			// in the middle
+			return 8;
+		}
+
+		record Coordinate(int row, int col) {
+		}
+	}
+
+	private static class Coupon implements Comparable<Coupon> {
+		private int pointees = 1;
+
+		private void transferPointee(Coupon to) {
+			this.pointees--;
+			to.pointees++;
+		}
+
+		@Override
+		public int compareTo(Coupon o) {
+			return Integer.compare(pointees, o.pointees);
+		}
 	}
 }
